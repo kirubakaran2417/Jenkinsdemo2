94bdf3ee1f95e10059f24d8214780b18573c4c0b
k.kirubakaran <k.kirubakaran@stackroute.in>
2023-12-31 20:20:31 +0530

    first commit

---
+++ src/test/java/org/example/Sampletestcase.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class Sampletestcase {
+
+    //create a test method
+    @Test
+    public void test() {
+        System.out.println("Hello World");
+    }
+}
